d429a64804c076e8353f18e1edca456b61ded215
Jongho Kim <april9288@gmail.com>
2019-03-30 16:03:26 -0700

    Updates

---
+++ precache-manifest.0b8efbefdb72de49424fe4fe8b6afccd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2153b130245778df3896",
+    "url": "/youMedia/static/js/runtime~main.7e334da1.js"
+  },
+  {
+    "revision": "6b12d6bfb09a997a42cb",
+    "url": "/youMedia/static/js/main.2ddc1273.chunk.js"
+  },
+  {
+    "revision": "78ae13bb09c9920356bc",
+    "url": "/youMedia/static/js/2.89049148.chunk.js"
+  },
+  {
+    "revision": "6b12d6bfb09a997a42cb",
+    "url": "/youMedia/static/css/main.a7d1bebd.chunk.css"
+  },
+  {
+    "revision": "73e7fcdba54a57b48f121bbf2f55739a",
+    "url": "/youMedia/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -11,10 +11,10 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/youMedia/precache-manifest.f1c780330643748f7625d150fef5153f.js"
+  "/youMedia/precache-manifest.0b8efbefdb72de49424fe4fe8b6afccd.js"
 );
 
 workbox.clientsClaim();
